108a85e85f60ef9d007911650cad2a8b2b6756ef
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2026-06-22 19:20:12 -0400

    refactor(daemon): remove adapter package and substrate services

--- features/adapter/src/sdk.ts
+++
@@ -1,8 +0,0 @@
-import { defineSdkPlugin } from "@goddard-ai/sdk-plugin"
-
-import { adapterIpcRoutes } from "./daemon-ipc.ts"
-
-export const adapterSdkPlugin = defineSdkPlugin({
-  name: "adapter",
-  ipcRoutes: adapterIpcRoutes,
-})
